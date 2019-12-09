bcb095ac671642d353d4515c13d54c809d355c8f
MrIlyaneX <mistyurin.ilya.im@gmail.com>
2019-12-09 21:44:01 +0300

    tests with Vue.js

--- js/mainVue.js
+++ js/mainVue.js
@@ -1,7 +1,14 @@
+Vue.component('app-left-block',{
+	data: function  (argument) {
+		return 1
+	},
+	template: '<div id = "main_left_block"><div id="left_block" v-show = "show"><a href="../index.html" title="На главную" id="leftContact" style="margin-top: 80px;">Paper Project</a><a href="../about.html" title="Информация" id="leftContact">О нас</a><span id="../leftContact"><a href="reg.html" title="Зарегестрироваться" onclick="heyReg()">Регистрация</a></span><span id="leftContact"><a href="../comein.html" title="Вход">Войти</a></span></div></div>'
+})
+
 new Vue({
-    el: '#app',
+    el: '#main_left_block',
     data: {
-        title: "Hey"
+    	show: true
     },
     components:{
 
