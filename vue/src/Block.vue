017c41da20045084572f2b542f673062ebf02b05
MrIlyaneX <45830744+MrIlyaneX@users.noreply.github.com>
2019-12-10 13:28:02 +0300

    .

---
+++ vue/src/Block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="block" v-on:click="show = !show">
+    <div v-if="!show" id="left_block">
+        <a href="index.html" title="На главную" id="leftContact">Paper Project</a>
+        <a href="about.html" title="Информация" id="leftContact">О нас</a>
+        <span id="leftContact"><a href="reg.html" title="Зарегестрироваться" onclick="heyReg()">Регистрация</a></span>
+        <span id="leftContact"><a href="comein.html" title="Вход">Войти</a></span>
+    </div>
+    <div v-else="show" id = "ClosedBlock"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'block',
+  show: true,
+  methods: {
+    CloseBlock(event) {
+      return !show
+    }
+  }
+}
+</script>
+
+<style scoped="Block.vue">
+#ClosedBlock {
+    background: #151a23;
+    color: #fff;
+    position: fixed;
+    width: 70px;
+    top: 0;
+    bottom: 0;
+    z-index: 1000;
+    left: 0;
+}
+</style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Block from './Block.vue'
+
+Vue.component('app_block', Block);
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+  el: '#block',
+  render: h => h(Block)
 })
